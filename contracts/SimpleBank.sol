9e4c9e5506cc1bf999232f3719a7e8b1817bc6c9
Fabio Correa <feamcor@gmail.com>
2018-12-12 21:43:25 -0300

    Exercise resolution. First commit.
    feamcor@alita [~/Desktop/simple-bank-feamcor/contracts] 0 1071 $ truffle test
    Compiling ./contracts/Migrations.sol...
    Compiling ./contracts/SimpleBank.sol...
    
      Contract: SimpleBank
        ✓ mark addresses as enrolled (149ms)
        ✓ should deposit correct amount (193ms)
        ✓ should withdraw correct amount (135ms)
    
      3 passing (518ms)

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -1,54 +1,54 @@
 pragma solidity ^0.4.21;
 contract SimpleBank {
-
-    //
-    // State variables
-    //
-    
+  
     /* Fill in the keyword. Hint: We want to protect our users balance from other contracts*/
-    mapping (address => uint) balances;
+    mapping (address => uint) private balances;
     
     /* Fill in the keyword. We want to create a getter function and allow contracts to be able to see if a user is enrolled.  */
-    mapping (address => bool) enrolled;
+    mapping (address => bool) public enrolled;
 
     /* Let's make sure everyone knows who owns the bank. Use the appropriate keyword for this*/
-    address owner;
-    
-    //
-    // Events - publicize actions to external listeners
-    //
-    
+    address public owner;
+        
     /* Add an argument for this event, an accountAddress */
-    event LogEnrolled();
+    event LogEnrolled(address accountAddress);
 
     /* Add 2 arguments for this event, an accountAddress and an amount */
-    event LogDepositMade();
+    event LogDepositMade(address accountAddress, uint amount);
 
     /* Create an event called LogWithdrawal */
     /* Add 3 arguments for this event, an accountAddress, withdrawAmount and a newBalance */
+    event LogWithdrawal(address accountAddress, uint withdrawAmount, uint newBalance);
 
-
-    //
-    // Functions
-    //
+    modifier isCustomerEnrolled() {
+        require(enrolled[msg.sender] == true, "customer must be enrolled already");
+        _;
+    }
 
     /* Use the appropriate global variable to get the sender of the transaction */
-    constructor() {
+    constructor() public {
         /* Set the owner to the creator of this contract */
+        owner = msg.sender;
     }
 
     /// @notice Get balance
     /// @return The balance of the user
     // A SPECIAL KEYWORD prevents function from editing state variables;
     // allows function to run locally/off blockchain
-    function balance() public returns (uint) {
+    function balance() public view isCustomerEnrolled() returns (uint) {
         /* Get the balance of the sender of this transaction */
+        return balances[msg.sender];
     }
 
     /// @notice Enroll a customer with the bank
     /// @return The users enrolled status
     // Emit the appropriate event
-    function enroll() public returns (bool){
+    function enroll() public returns (bool) {
+        require(enrolled[msg.sender] == false, "customer must not be enrolled yet");
+        enrolled[msg.sender] = true;
+        balances[msg.sender] = 0;
+        emit LogEnrolled(msg.sender);
+        return true;
     }
 
     /// @notice Deposit ether into bank
@@ -56,9 +56,12 @@ contract SimpleBank {
     // Add the appropriate keyword so that this function can receive ether
     // Use the appropriate global variables to get the transaction sender and value
     // Emit the appropriate event    
-    function deposit() public returns (uint) {
+    function deposit() public payable isCustomerEnrolled() returns (uint) {
         /* Add the amount to the user's balance, call the event associated with a deposit,
           then return the balance of the user */
+        balances[msg.sender] += msg.value;
+        emit LogDepositMade(msg.sender, msg.value);
+        return balances[msg.sender];
     }
 
     /// @notice Withdraw ether from bank
@@ -66,11 +69,17 @@ contract SimpleBank {
     /// @param withdrawAmount amount you want to withdraw
     /// @return The balance remaining for the user
     // Emit the appropriate event    
-    function withdraw(uint withdrawAmount) public returns (uint) {
+    function withdraw(uint withdrawAmount) public isCustomerEnrolled() returns (uint) {
         /* If the sender's balance is at least the amount they want to withdraw,
            Subtract the amount from the sender's balance, and try to send that amount of ether
            to the user attempting to withdraw. 
            return the user's balance.*/
+        require(withdrawAmount > 0, "withdraw amount must be greater than zero");
+        require(withdrawAmount <= balances[msg.sender], "withdraw amount must be less than balance");
+        balances[msg.sender] -= withdrawAmount;
+        msg.sender.transfer(withdrawAmount);
+        emit LogWithdrawal(msg.sender, withdrawAmount, balances[msg.sender]);
+        return balances[msg.sender]; 
     }
 
     // Fallback function - Called if other functions don't match call or
@@ -78,7 +87,7 @@ contract SimpleBank {
     // Typically, called when invalid data is sent
     // Added so ether sent to this contract is reverted if the contract fails
     // otherwise, the sender's money is transferred to contract
-    function() {
-        revert();
+    function() external {
+        revert("invalid method. reverting ether");
     }
 }
